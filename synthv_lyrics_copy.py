2c7403df7a26c2d816a71355aaacf33887dbbca1
schef <zvanstefan@gmail.com>
2019-04-19 10:13:14 +0200

    added click

--- synthv_lyrics_copy.py
+++ synthv_lyrics_copy.py
@@ -2,11 +2,49 @@
 
 import json
 import sys
+import click
 
 ONE_BEAT = 705600000
 BEATS_IN_BAR = 4
 THIRTYSECOND_NOTES_IN_BEAT = 8
 
+class txt:
+    ENDC = '\033[0m'
+    CBOLD = '\33[1m'
+    CITALIC = '\33[3m'
+    CURL = '\33[4m'
+    CBLINK = '\33[5m'
+    CBLINK2 = '\33[6m'
+    CSELECTED = '\33[7m'
+
+    CBLACK = '\33[30m'
+    CRED = '\33[31m'
+    CGREEN = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE = '\33[36m'
+    CWHITE = '\33[37m'
+
+    CBLACKBG = '\33[40m'
+    CREDBG = '\33[41m'
+    CGREENBG = '\33[42m'
+    CYELLOWBG = '\33[43m'
+    CBLUEBG = '\33[44m'
+    CVIOLETBG = '\33[45m'
+    CBEIGEBG = '\33[46m'
+    CWHITEBG = '\33[47m'
+
+    CGREY = '\33[90m'
+    CRED2 = '\33[91m'
+    CGREEN2 = '\33[92m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2 = '\33[94m'
+    CVIOLET2 = '\33[95m'
+    CBEIGE2 = '\33[96m'
+    CWHITE2 = '\33[97m'
+
+    TAB = '    '
 
 def read_file(file_name):
     with open(file_name, "r") as read_file:
@@ -68,41 +106,58 @@ def edit_lyrics(data, start_bar, start_beat, end_bar, end_beat, track_from, trac
     data = set_track_by_name(data, track_to_data, track_to)
     return data
 
+@click.group()
+def main():
+    pass
+
+@click.command()
+@click.argument('infile', type=click.Path(exists=True))
+def info(infile):
+    click.echo(txt.CBOLD + "Info:" + txt.ENDC + " " + infile)
+    infile_content = read_file(infile)
+
+    click.echo(txt.CBOLD + "Tracks:" + txt.ENDC)
+    track_names = get_track_names(infile_content)
+    for name in track_names:
+        click.echo(txt.TAB + name)
+
+main.add_command(info)
 
 if __name__ == "__main__":
+    main()
     # if not (len(sys.argv) > 2):
         # print("Usage:")
         # print("    " + sys.argv[0] + " <infile> <outfile>")
         # sys.exit(0)
-    infile = sys.argv[1]
-    data = read_file(infile)
+    # infile = sys.argv[1]
+    # data = read_file(infile)
 
-    start_bar = input("Start bar [0]: ")
-    if not start_bar:
-        start_bar = 0
-    start_bar = int(start_bar)
+    # start_bar = input("Start bar [0]: ")
+    # if not start_bar:
+    #     start_bar = 0
+    # start_bar = int(start_bar)
 
-    start_beat = input("Start beat [0]: ")
-    if not start_beat:
-        start_beat = 0
-    start_beat = int(start_beat)
+    # start_beat = input("Start beat [0]: ")
+    # if not start_beat:
+    #     start_beat = 0
+    # start_beat = int(start_beat)
 
-    end_bar = input("End bar [10]: ")
-    if not end_bar:
-        end_bar = 10
-    end_bar = int(end_bar)
+    # end_bar = input("End bar [10]: ")
+    # if not end_bar:
+    #     end_bar = 10
+    # end_bar = int(end_bar)
 
-    end_beat = input("End beat [0]: ")
-    if not end_beat:
-        end_beat = 10
-    end_beat = int(end_beat)
+    # end_beat = input("End beat [0]: ")
+    # if not end_beat:
+    #     end_beat = 10
+    # end_beat = int(end_beat)
 
-    track_names = get_track_names(data)
-    print(track_names)
+    # track_names = get_track_names(data)
+    # print(track_names)
 
-    track_from = input("Track name to copy from [" + track_names[0] + "]: ")
-    if not track_from:
-        track_from = track_names[0]
+    # track_from = input("Track name to copy from [" + track_names[0] + "]: ")
+    # if not track_from:
+    #     track_from = track_names[0]
 
     # track_to = input("Track name to copy to [" + track_names[1] + "]: ")
     # if not track_to:
@@ -112,10 +167,10 @@ if __name__ == "__main__":
         # print("Unvalid track name")
         # sys.exit(0)
 
-        track_to = []
+    #     track_to = []
 
-    data_edit = edit_lyrics(data, start_bar, start_beat,
-                            end_bar, end_beat, track_from, track_to)
+    # data_edit = edit_lyrics(data, start_bar, start_beat,
+    #                         end_bar, end_beat, track_from, track_to)
 
     # outfile = sys.argv[2]
     # write_file(outfile, data_edit)
