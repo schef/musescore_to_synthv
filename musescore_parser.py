9ac70b08b72c77a8e4a2b125f6dd42d429c45aba
schef <zvanstefan@gmail.com>
2019-05-20 21:03:40 +0200

    decoupled parser

---
+++ musescore_parser.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import xml.etree.ElementTree as ET
+from txt_colors import txt
+
+set_time_signature_cb = None
+set_pitch_cb = None
+set_rest_cb = None
+
+def set_time_signature(n, d):
+    # print(txt.CRED + "set_time_signature" + txt.CEND, n, d)
+    global set_time_signature_cb
+    if set_time_signature_cb:
+        set_time_signature_cb(n, d)
+
+def parse_time_signature(v):
+    n = d = ""
+    for t in v.findall("./*"):
+        print(txt.TAB * 3, t.tag)
+        if (t.tag == "sigN"):
+            n = t.text
+        elif (t.tag == "sigD"):
+            d = t.text
+    set_time_signature(n, d)
+
+def set_pitch(pitch, duration):
+    # print(txt.CRED + "set_pitch" + txt.CEND, pitch, duration)
+    global set_pitch_cb
+    if set_pitch_cb:
+        set_pitch_cb(pitch, duration)
+
+def parse_chord(v):
+    duration = ""
+    for c in v.findall("./*"):
+        print(txt.TAB * 3, c.tag)
+        if (c.tag == "durationType"):
+            duration = c.text
+        if (c.tag == "Note"):
+            for n in c.findall("./*"):
+                if (n.tag == "pitch"):
+                    set_pitch(n.text, duration)
+
+def set_rest(duration):
+    # print(txt.CRED + "set_rest" + txt.CEND, duration)
+    global set_rest_cb
+    if set_rest_cb:
+        set_rest_cb(duration)
+
+def parse_rest(v):
+    for r in v.findall("./*"):
+        print(txt.TAB * 3, r.tag)
+        if (r.tag == "durationType"):
+            set_rest(r.text)
+
+def parse_root(r):
+    for s in r.findall("./Score/Staff"):
+        print("Staff", s.attrib)
+        m_count = 0
+        for m in s.findall("./Measure"):
+            print(txt.TAB, m.tag, m_count)
+            m_count += 1
+            for v in m.findall("./voice/*"):
+                print(txt.TAB * 2, v.tag)
+                if (v.tag == "TimeSig"):
+                    parse_time_signature(v)
+                elif (v.tag == "Chord"):
+                    parse_chord(v)
+                elif (v.tag == "Rest"):
+                    parse_rest(v)
+
+def parse_xml(xml_file, set_time_signature_func = None, set_pitch_func = None, set_rest_func = None):
+    if (set_time_signature_func):
+        global set_time_signature_cb
+        set_time_signature_cb = set_time_signature_func
+    if (set_pitch_func):
+        global set_pitch_cb
+        set_pitch_cb = set_pitch_func
+    if (set_rest_func):
+        global set_rest_cb
+        set_rest_cb = set_rest_func
+
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    parse_root(root)
--- simple_parse.py
+++ simple_parse.py
@@ -1,44 +1,8 @@
 #!/usr/bin/env python
 
-import xml.etree.ElementTree as ET
+from txt_colors import txt
+import musescore_parser as MP
 
-class txt:
-    CEND = '\033[0m'
-    CBOLD = '\33[1m'
-    CITALIC = '\33[3m'
-    CURL = '\33[4m'
-    CBLINK = '\33[5m'
-    CBLINK2 = '\33[6m'
-    CSELECTED = '\33[7m'
-
-    CBLACK = '\33[30m'
-    CRED = '\33[31m'
-    CGREEN = '\33[32m'
-    CYELLOW = '\33[33m'
-    CBLUE = '\33[34m'
-    CVIOLET = '\33[35m'
-    CBEIGE = '\33[36m'
-    CWHITE = '\33[37m'
-
-    CBLACKBG = '\33[40m'
-    CREDBG = '\33[41m'
-    CGREENBG = '\33[42m'
-    CYELLOWBG = '\33[43m'
-    CBLUEBG = '\33[44m'
-    CVIOLETBG = '\33[45m'
-    CBEIGEBG = '\33[46m'
-    CWHITEBG = '\33[47m'
-
-    CGREY = '\33[90m'
-    CRED2 = '\33[91m'
-    CGREEN2 = '\33[92m'
-    CYELLOW2 = '\33[93m'
-    CBLUE2 = '\33[94m'
-    CVIOLET2 = '\33[95m'
-    CBEIGE2 = '\33[96m'
-    CWHITE2 = '\33[97m'
-
-    TAB = '  '
 
 time_signature_n = 0
 time_signature_d = 0
@@ -64,55 +28,10 @@ duration_type = {
 def set_time_signature(n, d):
     print(txt.CRED + "set_time_signature" + txt.CEND, n, d)
 
-def parse_time_signature(v):
-    n = d = ""
-    for t in v.findall("./*"):
-        print(txt.TAB * 3, t.tag)
-        if (t.tag == "sigN"):
-            n = t.text
-        elif (t.tag == "sigD"):
-            d = t.text
-    set_time_signature(n, d)
-
 def set_pitch(pitch, duration):
     print(txt.CRED + "set_pitch" + txt.CEND, pitch, duration)    
-
-def parse_chord(v):
-    duration = ""
-    for c in v.findall("./*"):
-        print(txt.TAB * 3, c.tag)
-        if (c.tag == "durationType"):
-            duration = c.text
-        if (c.tag == "Note"):
-            for n in c.findall("./*"):
-                if (n.tag == "pitch"):
-                    set_pitch(n.text, duration)
 
 def set_rest(duration):
     print(txt.CRED + "set_rest" + txt.CEND, duration)
 
-def parse_rest(v):
-    for r in v.findall("./*"):
-        print(txt.TAB * 3, r.tag)
-        if (r.tag == "durationType"):
-            set_rest(r.text)
-
-def parse_root(r):
-    for s in r.findall("./Score/Staff"):
-        print("Staff", s.attrib)
-        m_count = 0
-        for m in s.findall("./Measure"):
-            print(txt.TAB, m.tag, m_count)
-            m_count += 1
-            for v in m.findall("./voice/*"):
-                print(txt.TAB * 2, v.tag)
-                if (v.tag == "TimeSig"):
-                    parse_time_signature(v)
-                elif (v.tag == "Chord"):
-                    parse_chord(v)
-                elif (v.tag == "Rest"):
-                    parse_rest(v)
-
-tree = ET.parse('examples/musicxml.mscx')
-root = tree.getroot()
-parse_root(root)
+MP.parse_xml('examples/musicxml.mscx', set_time_signature, set_pitch, set_rest)
