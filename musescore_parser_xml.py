ab8c2c5967af40efad2ce1453501ce01b4af34b2
schef <zvanstefan@gmail.com>
2019-09-19 08:12:20 +0200

    added basic xml test

--- musescore_parser_xml.py
+++ musescore_parser_xml.py
@@ -2,14 +2,16 @@
 
 import xml.etree.ElementTree as ET
 
-def read_xml_root_from_file(file_name):
-    tree = ET.parse(file_name)
-    root = tree.getroot()
-    return root
+def read_xml_tree_from_file(file_name):
+    xml_tree = ET.parse(file_name)
+    return xml_tree
 
 def read_xml_root_from_string(xml_string):
     root = ET.fromstring(xml_string)
     return root
+
+def get_xml_root_from_xml_tree(xml_tree):
+    return xml_tree.getroot()
 
 def print_all(tag, tab):
     print(tab + tag.tag, tag.attrib, tag.text)
@@ -17,7 +19,10 @@ def print_all(tag, tab):
     for child in tag:
         print_all(child, tab)
 
+def write_xml_tree_to_file(xml_tree, file_name):
+    xml_tree.write(file_name)
 
 if __name__ == "__main__":
-    root = read_xml_root_from_file("./tests/xml/basic.xml")
-    print_all(root, "")
+    tree = read_xml_tree_from_file("./tests/xml/basic.xml")
+    root = get_xml_root_from_xml_tree(tree)
+    write_xml_tree_to_file(tree, "./tests/xml/basic_copy.xml")
